7061226ffae81f8e70b3342ce3fba6a35c2e0ce5
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2024-12-09 09:11:37 +0200

    Add Day0 template

---
+++ src/template/Day0.kt
@@ -0,0 +1,24 @@
+package template
+
+import println
+import readInput
+import readTestInput
+
+fun template() {
+    fun part1(inputs: List<String>): Long {
+        return 0L
+    }
+
+    fun part2(inputs: List<String>): Long {
+        return 0L
+    }
+
+    val testInput = readTestInput(2024, 0)
+    val actualInput = readInput(2024, 0)
+
+    part1(testInput).println()
+    part1(actualInput).println()
+
+    part2(testInput).println()
+    part2(actualInput).println()
+}
